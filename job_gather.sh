eab3142baee1548364e6f8e4197af1b23e54cf08
Numi SC <numisveinsson@gmail.com>
2024-01-21 14:32:52 -0800

    new slurm for savio

---
+++ job_gather.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=auto_cent
+#
+# Account:
+#SBATCH --account=fc_biome
+#
+# Partition:
+#SBATCH --partition=savio3_htc
+#
+# QoS:
+#SBATCH --qos=savio_normal
+#
+# Number of nodes:
+#SBATCH --nodes=1
+#
+# Number of tasks needed for use case (example):
+#SBATCH --ntasks=1
+#
+# Processors per task:
+#SBATCH --cpus-per-task=2
+#
+# Wall clock limit:
+#SBATCH --time=72:00:00
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=numi@berkeley.edu
+#
+## Command(s) to run (example):
+module load gcc
+module load cuda/10.0
+module load cudnn/7.5
+source activate /global/scratch/users/numi/environments/seqseg2
+
+python3  gather_sampling_data.py
