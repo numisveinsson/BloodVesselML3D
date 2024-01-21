0dd8bf5afa1e2bb5403b32990589f01099cbea0f
numisveinsson <numisveinsson@gmail.com>
2024-01-21 11:38:28 -0800

    deleting old functionality

--- Data/view_data.py
+++ Data/view_data.py
@@ -5,6 +5,38 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+def visualize(train_dataset, number):
+    """Visualize a sample from data set, specified by number"""
+    data = train_dataset.take(number)
+    images, labels = list(data)[0]
+    images = images.numpy()
+    image = images[0]
+    print("Dimension of the CT scan is:", image.shape)
+    plt.imshow(np.squeeze(image[:, :, 30]), cmap="gray")
+
+def plot_slices(num_rows, num_columns, width, height, data):
+    """Plot a montage of 20 CT slices"""
+    data = np.rot90(np.array(data))
+    data = np.transpose(data)
+    data = np.reshape(data, (num_rows, num_columns, width, height))
+    rows_data, columns_data = data.shape[0], data.shape[1]
+    heights = [slc[0].shape[0] for slc in data]
+    widths = [slc.shape[1] for slc in data[0]]
+    fig_width = 12.0
+    fig_height = fig_width * sum(heights) / sum(widths)
+    f, axarr = plt.subplots(
+        rows_data,
+        columns_data,
+        figsize=(fig_width, fig_height),
+        gridspec_kw={"height_ratios": heights},
+    )
+    for i in range(rows_data):
+        for j in range(columns_data):
+            axarr[i, j].imshow(data[i][j], cmap="gray")
+            axarr[i, j].axis("off")
+    plt.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+    plt.show()
+
 def myshow(img, seg, pred=None, title=None):
     """
     Function to display 2D sitk images together
