e6fafe8d63751151b86fd74bdca18a30cfc53565
numisveinsson <numisveinsson@gmail.com>
2024-02-05 10:10:39 -0800

    gt as uint8

--- modules/sampling_functions.py
+++ modules/sampling_functions.py
@@ -822,6 +822,8 @@ def write_img(new_img, removed_seg, image_out_dir, seg_out_dir, case_name, N, n_
     if max_seg_value != 1:
         import pdb; pdb.set_trace()
         removed_seg /= float(max_seg_value*1.0)
+        # make image unsigned int, removed_seg is sitk image
+        removed_seg = sitk.Cast(removed_seg, sitk.sitkUInt8)
     print(f"Max seg value: {sitk.GetArrayFromImage(removed_seg).max()}")
     sitk.WriteImage(removed_seg, seg_out_dir + case_name +'_'+ str(N-n_old) +'_'+str(sub)+'.nii.gz')
     
