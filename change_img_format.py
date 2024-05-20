5dab4ec28f3f170716354771a1f321a5d586379f
numisveinsson <numisveinsson@gmail.com>
2024-05-20 11:04:10 -0700

    adding dicom reading

--- change_img_format.py
+++ change_img_format.py
@@ -35,13 +35,13 @@ if __name__=='__main__':
 
     # import pdb; pdb.set_trace()
 
-    input_format = '.vti'
-    output_format = '.nii.gz'
+    input_format = '.dcm'
+    output_format = '.mha'
     label = False # false if raw image
 
     rem_str = 'coroasocact_0'
 
-    data_folder = '/Users/numisveins/Documents/data_combo_paper/ct_data/test_images_for_combined/'
+    data_folder = '/Users/numisveins/Downloads/Phase_6/'
     out_folder = data_folder+'new_format/'
 
     imgs = os.listdir(data_folder)
@@ -54,35 +54,50 @@ if __name__=='__main__':
         print(e)
         imgs_old = os.listdir(out_folder)
 
-    for fn in imgs:
-        if fn.replace(input_format, output_format) in imgs_old:
-            print(f"File {fn} already exists in folder")
-            continue
-        else:
-            print(f"Converting file {fn} to new format {output_format}")
-        if input_format != '.vti' and output_format != '.vti':
-            img = sitk.ReadImage(data_folder+fn)
-            if label:
-                img = sitk.Cast(img, sitk.sitkUInt8)
-        elif input_format == '.vti':
-            if output_format == '.mha' or output_format == '.nii.gz':
-                img = change_vti_sitk(data_folder+fn)
-            else:   
-                img = vf.read_img(data_folder+fn).GetOutput()
-        else:
-            if input_format == '.mha':
-                img = change_mha_vti(data_folder+fn)
-        
-        if rem_str:
-            fn = fn.replace(rem_str, '')
-        
-        img_name = fn.replace(input_format, '')
-        # make int and remove 1 
-        # img_name = str(int(img_name)-1)
-        # img_name = img_name.zfill(2) + output_format
+    if input_format == '.dcm':
+        # it's a dicom folder with multiple files
+        # create a 3D image from the dicom files
+        print(f"Reading dicom files from {data_folder}")
+        reader = sitk.ImageSeriesReader()
+        dicom_names = reader.GetGDCMSeriesFileNames(data_folder)
+        reader.SetFileNames(dicom_names)
+        img = reader.Execute()
+        print(f"Saving 3D image to {out_folder+'3D_image'+output_format}")
+        sitk.WriteImage(img, out_folder+'3D_image'+output_format)
+    else:
+        # it's a single file
+        for fn in imgs:
+            if fn.replace(input_format, output_format) in imgs_old:
+                print(f"File {fn} already exists in folder")
+                continue
+            else:
+                print(f"Converting file {fn} to new format {output_format}")
+            if input_format != '.vti' and output_format != '.vti':
+                img = sitk.ReadImage(data_folder+fn)
+                if label:
+                    img = sitk.Cast(img, sitk.sitkUInt8)
+            elif input_format == '.vti':
+                if output_format == '.mha' or output_format == '.nii.gz':
+                    img = change_vti_sitk(data_folder+fn)
+                else:   
+                    img = vf.read_img(data_folder+fn).GetOutput()
+            elif output_format == '.vti':
+                if input_format == '.mha':
+                    img = change_mha_vti(data_folder+fn)
+            else:
+                print('Invalid input/output format')
+                break
+            
+            if rem_str:
+                fn = fn.replace(rem_str, '')
+            
+            img_name = fn.replace(input_format, '')
+            # make int and remove 1 
+            # img_name = str(int(img_name)-1)
+            # img_name = img_name.zfill(2) + output_format
 
-        print(f"Saving {img_name}")
-        if output_format != '.vti':
-            sitk.WriteImage(img, out_folder+img_name+output_format)
-        else:
-            vf.write_img(out_folder+img_name+output_format, img)
+            print(f"Saving {img_name}")
+            if output_format != '.vti':
+                sitk.WriteImage(img, out_folder+img_name+output_format)
+            else:
+                vf.write_img(out_folder+img_name+output_format, img)
