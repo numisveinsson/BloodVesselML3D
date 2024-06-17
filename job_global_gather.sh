fedef5f0b00848c6f8bcb4432daf35716a740db4
numisveinsson <numisveinsson@gmail.com>
2024-06-17 14:34:21 -0700

    add job for global data

---
+++ job_global_gather.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=gather_data
+#
+# Account:
+#SBATCH --account=fc_biome
+#
+# Partition:
+#SBATCH --partition=savio4_htc
+#
+# QoS:
+#SBATCH --qos=savio_normal
+#
+# Number of nodes:
+#SBATCH --nodes=1
+#
+# Number of tasks needed for use case (example):
+#SBATCH --ntasks=1
+#
+# Processors per task:
+#SBATCH --cpus-per-task=8
+#
+# Wall clock limit:
+#SBATCH --time=24:00:00
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=numi@berkeley.edu
+#
+## Command(s) to run (example):
+module load gcc
+module load cuda/10.0
+module load cudnn/7.5
+source activate /global/scratch/users/numi/environments/seqseg2
+cd /global/scratch/users/numi/BloodVesselML3D/
+
+python3  global/global_data.py \
+    -outdir /global/scratch/users/numi/vascular_data_3d/extraction_output/vmr_aortas_perc_data_015/ \
+    -config_name global_more_samples \
+    -perc_dataset 0.15 \
